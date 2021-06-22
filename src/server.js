03c1053f8ae8d6b43252840d42fa0372a297ec5f
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-06-22 19:28:05 +0700

    implement postgres

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+const songs = require("./api/songs");
+const SongsService = require("./services/postgres/SongsService");
+const SongsValidator = require("./validator/songs");
 
 const init = async () => {
+  const songsService = new SongsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -11,6 +15,14 @@ const init = async () => {
       cors: {
         origin: ["*"],
       },
+    },
+  });
+
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
     },
   });
 
