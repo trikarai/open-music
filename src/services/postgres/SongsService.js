1bc36759cc6078df65b1a810b0fc2776fc8b2ebb
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-07-02 12:01:27 +0700

    add  prefix “song-”  on SongService id

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -11,7 +11,7 @@ class SongsService {
   }
 
   async addSong({ title, year, performer, genre, duration }) {
-    const id = nanoid(16);
+    const id = `song-${nanoid(16)}`;
     const insertedAt = new Date().toISOString();
     const updatedAt = insertedAt;
 
