9841052738df150e1bd19098359e7e4f745ae8d8
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-07-02 19:12:24 +0700

    register users plugin

--- src/server.js
+++ src/server.js
@@ -2,13 +2,22 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
-const songs = require("./api/songs");
 const ClientError = require("./exceptions/ClientError");
+
+// songs
+const songs = require("./api/songs");
 const SongsService = require("./services/postgres/SongsService");
 const SongsValidator = require("./validator/songs");
 
+// users
+const users = require("./api/users");
+const UsersService = require("./services/postgres/UsersService");
+const UsersValidator = require("./validator/users");
+
 const init = async () => {
   const songsService = new SongsService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -33,13 +42,22 @@ const init = async () => {
     return response.continue || response;
   });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
