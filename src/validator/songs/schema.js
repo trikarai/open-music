6c4e3ba59965e02a3d39812c1c9872204ad3ea53
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-07-02 12:00:59 +0700

    add min/max integer validation on song schema

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const SongsPayloadSchema = Joi.object({
   title: Joi.string().required(),
-  year: Joi.number().required(),
+  year: Joi.number().integer().min(1900).max(2021).required(),
   performer: Joi.string().required(),
   genre: Joi.string().required(),
   duration: Joi.number().required(),
