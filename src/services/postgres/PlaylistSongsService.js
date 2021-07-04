e07284d7c9e2fd4414a56ca98a098588f0eb03e2
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-07-04 12:08:11 +0700

    fix playlistsong  service

--- src/services/postgres/PlaylistSongsService.js
+++ src/services/postgres/PlaylistSongsService.js
@@ -9,6 +9,8 @@ class PlaylistSongsService {
   }
 
   async addSongToPlayist(playlistId, songId) {
+    // this.verifySongExist(songId);
+
     const id = `playlistsong-${nanoid(16)}`;
 
     const query = {
@@ -18,10 +20,45 @@ class PlaylistSongsService {
 
     const result = await this._pool.query(query);
     if (!result.rows[0].id) {
-      throw new InvariantError("Playlist gagal ditambahkan");
+      throw new InvariantError("Lagu gagal ditambahkan ke dalam playlist");
     }
 
     return result.rows[0].id;
+  }
+
+  async getPlaylistsongs(playlistId) {
+    const query = {
+      text: "SELECT songs.id, songs.title, songs.performer FROM playlistsongs JOIN songs ON songs.id = playlistsongs.song_id  WHERE playlist_id = $1",
+      values: [playlistId],
+    };
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
+
+  async deleteSongFromPlaylist(songId) {
+    const query = {
+      text: "DELETE FROM playlistsongs WHERE song_id = $1",
+      values: [songId],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError(
+        "Lagu gagal dihapus dari playlist. Id tidak ditemukan"
+      );
+    }
+  }
+
+  async verifySongExist(id) {
+    const query = {
+      text: "SELECT * FROM songs WHERE id = $1",
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError("Lagu tidak ditemukan");
+    }
   }
 }
 
