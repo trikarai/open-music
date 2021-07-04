5ec9bf58845732142e4bceda744ef57007307e6f
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-07-04 15:03:14 +0700

    Update 1625303191321_create-table-collaborations.js

--- migrations/1625303191321_create-table-collaborations.js
+++ migrations/1625303191321_create-table-collaborations.js
@@ -17,6 +17,23 @@ exports.up = (pgm) => {
       notNull: true,
     },
   });
+  pgm.addConstraint(
+    "collaborations",
+    "unique_playlist_id_and_user_id",
+    "UNIQUE(playlist_id, user_id)"
+  );
+
+  // memberikan constraint foreign key pada kolom playlist_id dan user_id terhadap notes.id dan users.id
+  pgm.addConstraint(
+    "collaborations",
+    "fk_collaborations.playlist_id_playlists.id",
+    "FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE"
+  );
+  pgm.addConstraint(
+    "collaborations",
+    "fk_collaborations.user_id_users.id",
+    "FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE"
+  );
 };
 
 exports.down = (pgm) => {
