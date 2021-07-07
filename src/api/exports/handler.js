a192711de75ecf7b0bd4d61e88c12a1901890755
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-07-07 10:56:57 +0700

    export playlists implemented

---
+++ src/api/exports/handler.js
@@ -0,0 +1,47 @@
+class ExportsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+    this.postExportPlaylistsHandler = this.postExportPlaylistsHandler.bind(this);
+  }
+
+  async postExportPlaylistsHandler(request, h) {
+    try {
+      this._validator.validateExportPlaylistsPayload(request.payload);
+      const message = {
+        userId: request.auth.credentials.id,
+        targetEmail: request.payload.targetEmail,
+      };
+
+      await this._service.sendMessage(
+        "export:playlists",
+        JSON.stringify(message)
+      );
+      const response = h.response({
+        status: "success",
+        message: "Permintaan Anda sedang kami proses",
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = ExportsHandler;
