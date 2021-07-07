c27d37d1315d4fd9286cd7a925b7857b7fdac12f
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-07-07 11:40:35 +0700

    implement uploads

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
 const Jwt = require("@hapi/jwt");
+const path = require("path");
 
 const ClientError = require("./exceptions/ClientError");
 
@@ -41,6 +42,11 @@ const _exports = require("./api/exports");
 const ProducerService = require("./services/rabbitmq/ProducerService");
 const ExportsValidator = require("./validator/exports");
 
+// uploads
+const uploads = require("./api/uploads");
+const StorageService = require("./services/storage/StorageService");
+const UploadsValidator = require("./validator/uploads");
+
 const init = async () => {
   const songsService = new SongsService();
   const usersService = new UsersService();
@@ -48,6 +54,9 @@ const init = async () => {
   const collaborationsService = new CollaborationsService();
   const playlistsService = new PlaylistsService(collaborationsService);
   const playlistsongsService = new PlaylistSongsService(collaborationsService);
+  const storageService = new StorageService(
+    path.resolve(__dirname, "api/uploads/file/images")
+  );
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -146,6 +155,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
