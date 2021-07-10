95c671d341f662dffe25e8907f148cb223406627
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-07-10 15:27:18 +0700

    fix upload get

--- src/api/uploads/routes.js
+++ src/api/uploads/routes.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const routes = (handler) => [
   {
     method: "POST",
@@ -9,6 +11,15 @@ const routes = (handler) => [
         allow: "multipart/form-data",
         multipart: true,
         output: "stream",
+      },
+    },
+  },
+  {
+    method: "GET",
+    path: "/upload/{param*}",
+    handler: {
+      directory: {
+        path: path.resolve(__dirname, "file"),
       },
     },
   },
